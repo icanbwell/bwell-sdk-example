bfcc5e5d089c1dd771bc6a242c0a219947c46424
Arun kumar <arun.kumar@icanbwell.com>
2023-12-26 21:41:44 +0530

    EA-907-sample-android-app-integrate-with-1.0.0-Snapshot

--- bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/activities/ui/labs/LabsFragment.kt
+++ bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/activities/ui/labs/LabsFragment.kt
@@ -1,8 +1,8 @@
 package com.bwell.sampleapp.activities.ui.labs
 
+import android.annotation.SuppressLint
 import android.graphics.Typeface
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -29,10 +29,7 @@ import com.bwell.sampleapp.viewmodel.LabsViewModelFactory
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.launch
 
-import com.bwell.sampleapp.model.LabsListItems
 import com.bwell.sampleapp.utils.parseDateStringToDate
-import com.bwell.sampleapp.viewmodel.SharedViewModelFactory
-import java.util.Date
 
 class LabsFragment : Fragment(), View.OnClickListener {
 
@@ -73,7 +70,7 @@ class LabsFragment : Fragment(), View.OnClickListener {
         viewLifecycleOwner.lifecycleScope.launch {
             labsViewModel.labsResults.take(1).collect { result ->
                 if (result != null) {
-                    Log.e("result","result-"+result)
+                    Log.e("result", "result-$result")
                     when (result) {
                         is BWellResult.ResourceCollection -> {
                             val dataList = result.data
@@ -90,8 +87,8 @@ class LabsFragment : Fragment(), View.OnClickListener {
     private fun setLabsAdapter(dataList: List<Observation>?) {
         val adapter = LabsListAdapter(dataList)
         adapter.onItemClicked = { selectedLabType ->
-            binding.includelabsData.labsFragment.visibility = View.GONE;
-            binding.includeLabsDetail.labDetailFragment.visibility = View.VISIBLE;
+            binding.includelabsData.labsFragment.visibility = View.GONE
+            binding.includeLabsDetail.labDetailFragment.visibility = View.VISIBLE
             showLabDetailedView(selectedLabType)
             showLabKnowledgeView(selectedLabType)
         }
@@ -99,6 +96,7 @@ class LabsFragment : Fragment(), View.OnClickListener {
         binding.includelabsData.rvLabs.adapter = adapter
     }
 
+    @SuppressLint("SetTextI18n")
     private fun showLabDetailedView(selectedLabType: Observation?) {
         binding.includeLabsDetail.labDataLl.removeAllViews()
         val id = selectedLabType?.id
@@ -120,16 +118,17 @@ class LabsFragment : Fragment(), View.OnClickListener {
         viewLifecycleOwner.lifecycleScope.launch {
             labsViewModel.labsDetailResults.take(1).collect { result ->
                 if (result != null) {
-                    Log.d("result","result"+result)
+                    Log.d("result", "result$result")
                     when (result) {
                         is BWellResult.ResourceCollection -> {
                             val dataList = result.data
-                            Log.d("dataList","dataList"+dataList)
+                            Log.d("dataList", "dataList$dataList")
                             val details:Observation? = dataList?.get(0)
                             binding.includeLabsDetail.typeText.text = details?.code?.text
-                            binding.includeLabsDetail.dateText.text = "as of "+details?.effectiveDateTime?.toString()?.let { formatDate(it) } ?: "---"
+                            binding.includeLabsDetail.dateText.text =
+                                ("as of " + details?.effectiveDateTime?.let { formatDate(it) })
                             binding.includeLabsDetail.organizationName.text = "from "+details?.meta?.security?.get(0)?.system
-                            addTextField(details?.effectiveDateTime?.toString()?.let { formatDate(it) } ?: "---",false)
+                            addTextField(details?.effectiveDateTime?.let { formatDate(it) } ?: "---",false)
                             addTextField(details?.interpretation?.get(0)?.text.toString(),false)
                             addTextField(resources.getString(R.string.healthy_range),true)
                             addTextField(details?.referenceRange?.get(0)?.text.toString(),false)
@@ -169,7 +168,7 @@ class LabsFragment : Fragment(), View.OnClickListener {
         viewLifecycleOwner.lifecycleScope.launch {
             labsViewModel.labsKnowledgeResults.take(1).collect { result ->
                 if (result != null) {
-                    Log.d("result","result"+result)
+                    Log.d("result", "result$result")
                     when (result) {
                         is BWellResult.SingleResource -> {
                             val data = result.data
@@ -193,8 +192,8 @@ class LabsFragment : Fragment(), View.OnClickListener {
     override fun onClick(view: View?) {
         when (view?.id) {
             R.id.leftArrowImageView -> {
-                binding.includelabsData.labsFragment.visibility = View.VISIBLE;
-                binding.includeLabsDetail.labDetailFragment.visibility = View.GONE;
+                binding.includelabsData.labsFragment.visibility = View.VISIBLE
+                binding.includeLabsDetail.labDetailFragment.visibility = View.GONE
             }
         }
     }
