07b79c7c761bee546fa8f47b1435483b597dad0f
grant <gespanet97@gmail.com>
2024-01-24 16:19:30 -0500

    EA-951 Add Base Resource Logging (#27)
    * add base resource logging
    
    * Updating to work with 0.12.0
    
    ---------
    
    Co-authored-by: katekolchier <77307368+katekolchier@users.noreply.github.com>
    Co-authored-by: Kate Kolchier <kate.kolchier@icanbwell.com>

--- bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/activities/ui/healthsummary/HealthSummaryCategoriesDataAdapter.kt
+++ bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/activities/ui/healthsummary/HealthSummaryCategoriesDataAdapter.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bwell.BWellSdk
 import com.bwell.common.models.domain.healthdata.healthsummary.allergyintolerance.AllergyIntoleranceGroup
 import com.bwell.common.models.domain.healthdata.healthsummary.careplan.CarePlanGroup
 import com.bwell.common.models.domain.healthdata.healthsummary.communication.Communication
@@ -11,9 +12,21 @@ import com.bwell.common.models.domain.healthdata.healthsummary.condition.Conditi
 import com.bwell.common.models.domain.healthdata.healthsummary.encounter.EncounterGroup
 import com.bwell.common.models.domain.healthdata.healthsummary.immunization.ImmunizationGroup
 import com.bwell.common.models.domain.healthdata.healthsummary.procedure.ProcedureGroup
+import com.bwell.common.models.domain.healthdata.observation.Observation
 import com.bwell.common.models.domain.healthdata.observation.ObservationComposition
+import com.bwell.common.models.responses.BWellResult
+import com.bwell.healthdata.healthsummary.requests.allergyintolerance.AllergyIntolerancesRequest
+import com.bwell.healthdata.healthsummary.requests.careplan.CarePlansRequest
+import com.bwell.healthdata.healthsummary.requests.condition.ConditionsRequest
+import com.bwell.healthdata.healthsummary.requests.encounter.EncountersRequest
+import com.bwell.healthdata.healthsummary.requests.immunization.ImmunizationsRequest
+import com.bwell.healthdata.healthsummary.requests.procedure.ProcedureRequest
+import com.bwell.healthdata.healthsummary.requests.vitalsign.VitalSignsRequest
 import com.bwell.sampleapp.databinding.HealthSummaryCategoriesItemsViewBinding
 import com.bwell.sampleapp.utils.formatDate
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlin.reflect.full.memberProperties
 
 /*
 *Display the Labs List in RecyclerView
@@ -98,7 +111,61 @@ class HealthSummaryCategoriesDataAdapter<T>(private val launches: List<T>?) :
             onEndOfListReached?.invoke()
         }
         holder.binding.root.setOnClickListener {
-            onItemClicked?.invoke(launch)
+            val id = getId(launch)
+            when (launch) {
+                is CarePlanGroup -> {
+                    val carePlanRequest = CarePlansRequest.Builder().ids(listOf(id)).build()
+                    GlobalScope.launch {
+                        val carePlans = BWellSdk.health.getCarePlans(carePlanRequest) as BWellResult.ResourceCollection
+                        printProperties(carePlans.data?.get(position))
+                    }
+                }
+                is ImmunizationGroup ->{
+                    val immunizationRequest = ImmunizationsRequest.Builder().ids(listOf(id)).build()
+                    GlobalScope.launch {
+                        val immunizations = BWellSdk.health.getImmunizations(immunizationRequest) as BWellResult.ResourceCollection
+                        printProperties(immunizations.data?.get(position))
+                    }
+                }
+                is ProcedureGroup ->{
+                    val proceduresRequest = ProcedureRequest.Builder().ids(listOf(id)).build()
+                    GlobalScope.launch {
+                        val procedures = BWellSdk.health.getProcedures(proceduresRequest) as BWellResult.ResourceCollection
+                        printProperties(procedures.data?.get(position))
+                    }
+                }
+                is ObservationComposition ->{
+                    val vitalSignsRequest = VitalSignsRequest.Builder().ids(listOf(id)).build()
+                    GlobalScope.launch {
+                        val vitalSigns = BWellSdk.health.getVitalSigns(vitalSignsRequest) as BWellResult.ResourceCollection
+                        printProperties(vitalSigns.data?.get(position))
+                    }
+                }
+                is EncounterGroup ->{
+                    val encountersRequest = EncountersRequest.Builder().ids(listOf(id)).build()
+                    GlobalScope.launch {
+                        val encounters = BWellSdk.health.getEncounters(encountersRequest) as BWellResult.ResourceCollection
+                        printProperties(encounters.data?.get(position))
+                    }
+                }
+                is AllergyIntoleranceGroup ->{
+                    val allergyIntoleranceRequest = AllergyIntolerancesRequest.Builder().ids(listOf(id)).build()
+                    GlobalScope.launch {
+                        val allergyIntolerances = BWellSdk.health.getAllergyIntolerances(allergyIntoleranceRequest) as BWellResult.ResourceCollection
+                        printProperties(allergyIntolerances.data?.get(position))
+                    }
+                }
+                is Communication ->{
+                    // do nothing
+                }
+                is ConditionGroup ->{
+                    val conditionsRequest = ConditionsRequest.Builder().ids(listOf(id)).build()
+                    GlobalScope.launch {
+                        val conditions = BWellSdk.health.getConditions(conditionsRequest) as BWellResult.ResourceCollection
+                        printProperties(conditions.data?.get(position))
+                    }
+                }
+            }
         }
     }
 
@@ -126,6 +193,29 @@ class HealthSummaryCategoriesDataAdapter<T>(private val launches: List<T>?) :
             is ConditionGroup -> item.recordedDate.toString()
             else -> null
         }
+    }
+
+    private fun getId(item: T?): String {
+        return when (item) {
+            is CarePlanGroup -> item.id ?: ""
+            is ImmunizationGroup -> item.id ?: ""
+            is ProcedureGroup -> item.id ?: ""
+            is ObservationComposition -> item.id ?: ""
+            is EncounterGroup -> item.id ?: ""
+            is ConditionGroup -> item.id ?: ""
+            else -> ""
+        }
+    }
+
+
+    fun printProperties(obj: Any?) {
+        if (obj == null) {
+            return
+        }
+        val properties = obj::class.memberProperties.joinToString(", ") { prop ->
+            "${prop.name} = ${prop.call(obj)}"
+        }
+        println("${obj::class.simpleName}($properties)")
     }
 }
 
