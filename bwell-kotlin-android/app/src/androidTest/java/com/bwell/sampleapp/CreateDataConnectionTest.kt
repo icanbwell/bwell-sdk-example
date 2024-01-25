bf4bb657b44403bd566f8555c51802a0a76cd2da
Imran Qureshi <imranq2@hotmail.com>
2024-01-25 12:59:39 -0800

    update test

--- bwell-kotlin-android/app/src/androidTest/java/com/bwell/sampleapp/CreateDataConnectionTest.kt
+++ bwell-kotlin-android/app/src/androidTest/java/com/bwell/sampleapp/CreateDataConnectionTest.kt
@@ -1,20 +1,42 @@
 package com.bwell.sampleapp
 
+import android.view.View
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.bwell.sampleapp.activities.NavigationActivity
 import com.bwell.sampleapp.activities.ui.data_connections.DataConnectionsCategoriesListAdapter
+import org.hamcrest.Matcher
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.concurrent.TimeoutException
 
 @RunWith(AndroidJUnit4::class)
 class CreateDataConnectionTest {
+
+    fun waitForView(viewMatcher: Matcher<View>, timeout: Long): ViewInteraction {
+        val startTime = System.currentTimeMillis()
+        val endTime = startTime + timeout
+
+        do {
+            try {
+                Espresso.onView(viewMatcher).check(ViewAssertions.matches(isDisplayed()))
+                return Espresso.onView(viewMatcher) // If view is found return it
+            } catch (e: Exception) {
+                Thread.sleep(50)
+            }
+        } while (System.currentTimeMillis() < endTime)
+
+        throw TimeoutException("View with matcher $viewMatcher not found after $timeout milliseconds")
+    }
 
     @Test
     fun testCreateConnection() {
@@ -37,6 +59,16 @@ class CreateDataConnectionTest {
             .perform(ViewActions.click())
 
         Espresso.onView(ViewMatchers.withId(R.id.rv_suggested_data_connection_categories))
-            .perform(RecyclerViewActions.actionOnItemAtPosition<DataConnectionsCategoriesListAdapter.ViewHolder>(2, ViewActions.click()))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<DataConnectionsCategoriesListAdapter.ViewHolder>(
+                    2,
+                    ViewActions.click()
+                )
+            )
+
+        waitForView(withId(R.id.searchText), 5000) // waits up to 5 seconds
+
+        Espresso.onView(ViewMatchers.withId(R.id.searchText))
+            .perform(ViewActions.typeText("bwell"))
     }
 }
