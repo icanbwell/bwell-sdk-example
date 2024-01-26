487727040147b1f0a5701d00838b46e86188558b
katekolchier <77307368+katekolchier@users.noreply.github.com>
2024-01-26 10:39:30 -0600

    EA-1000 Updates for 0.13.0 (#31)
    * Testing Groups and Base resources
    
    * Testing Careplans

--- bwell-kotlin-android/app/build.gradle.kts
+++ bwell-kotlin-android/app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
 dependencies {
 
     // BWell SDK Usage
-    implementation("com.bwell:bwell-sdk-kotlin:0.12.0")
+    implementation("com.bwell:bwell-sdk-kotlin:0.13.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
