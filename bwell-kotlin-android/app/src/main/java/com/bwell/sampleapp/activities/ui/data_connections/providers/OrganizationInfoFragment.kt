611e0c1489a3f58ae0fddb948e407b9494f08bd8
Jerry Westberg <jerry.westberg@icanbwell.com>
2024-01-25 23:04:56 -0700

    updated OrganizationInfoFragment for different onClickProceed behavior based on auth connection type. Cleaned up warnings

--- bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/activities/ui/data_connections/providers/OrganizationInfoFragment.kt
+++ bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/activities/ui/data_connections/providers/OrganizationInfoFragment.kt
@@ -17,24 +17,23 @@ import androidx.lifecycle.lifecycleScope
 import com.bwell.common.models.domain.common.Organization
 import com.bwell.common.models.domain.search.Provider
 import com.bwell.common.models.responses.BWellResult
-import com.bwell.connections.requests.ConnectionCreateRequest
 import com.bwell.sampleapp.R
 import com.bwell.sampleapp.activities.ui.data_connections.DataConnectionsFragment
 import com.bwell.sampleapp.databinding.FragmentOrganizationInfoViewBinding
 import com.bwell.common.models.domain.data.DataSource
+import com.bwell.common.models.domain.data.enums.ConnectionCategory
 import com.bwell.sampleapp.viewmodel.DataConnectionsViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 
-class OrganizationInfoFragment<T>(organizationData: T?) : Fragment(),View.OnClickListener {
+class OrganizationInfoFragment<T>(entityData: T?) : Fragment(),View.OnClickListener {
 
     private var _binding: FragmentOrganizationInfoViewBinding? = null
-    private var organization: T? = organizationData
-    private var connectionType: String = ""
-    private var connectionId: String = ""
-    private var dataSource: DataSource? = null
+    private var entity: T = entityData ?: throw Exception("Cannot create view without entityData")
+    private var entityName: String = getName(entity)
+    private var entityId: String = getId(entity)
+    private var connectionType: String = getConnectionType(entity)
+    private var authType: ConnectionCategory? = null
+    private var dataSourceId: String? = null
     private lateinit var viewModel: DataConnectionsViewModel
 
 
@@ -48,57 +47,14 @@ class OrganizationInfoFragment<T>(organizationData: T?) : Fragment(),View.OnClic
     ): View {
         _binding = FragmentOrganizationInfoViewBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        val organization = organization
-        var name = ""
 
-        // Collect organization information
         val parentFragment = requireParentFragment()
-        val viewModel = ViewModelProvider(parentFragment).get(DataConnectionsViewModel::class.java)
+        viewModel = ViewModelProvider(parentFragment)[DataConnectionsViewModel::class.java]
 
-        val orgId = getId(organization)
-            ?: throw Exception("OrgId was null. Bad things are happening here.")
+        // Certain login components need customization based on entity info
+        showLogin()
 
-        lifecycleScope.launch {
-            val dataSource = getDataSource(orgId);
-
-            Log.d("dataSource.type", dataSource.type.toString())
-            Log.d("dataSource.name", dataSource.name)
-            Log.d("dataSource.id", dataSource.id)
-            Log.d("dataSource.category", dataSource.category.toString())
-
-            if(dataSource.category?.toString() == "OAUTH") {
-                val oauthUrl = getOAuthUrl(dataSource.id)
-                Log.d("oauthUrl", oauthUrl)
-            }
-        }
-
-        when (organization) {
-            is Organization?->{
-                connectionType = organization?.endpoint?.get(0)?.connectionType?.code.toString()
-                name = organization?.name.toString()
-            }
-            is Provider?->{
-                connectionType = organization?.endpoint?.get(0)?.connectionType?.code.toString()
-                name = organization?.content.toString()
-            }
-        }
-
-        Log.d("OrganizationInfoFragment", "Connection Type: $organization?.endpoint?.get")
-
-        binding.clinicNametxt.text =
-            "${resources.getString(R.string.connect_to)} ${name}"
-        if(connectionType.equals(resources.getString(R.string.open)))
-        {
-            binding.clinicDiscriptionTxt.text ="By providing  my "+
-                    "${name} ${resources.getString(R.string.clinic_info_hapi)}"
-            binding.editTextUsername.visibility = View.VISIBLE;
-            binding.passwordLayout.visibility = View.VISIBLE;
-        }else{
-            binding.clinicDiscriptionTxt.text =
-                "${name} ${resources.getString(R.string.clinic_discription)}"
-            binding.editTextUsername.visibility = View.GONE;
-            binding.passwordLayout.visibility = View.GONE;
-        }
+        // Login components that don't require customization based on entity
         binding.cancelTxt.setOnClickListener(this)
         binding.togglePassword.setOnClickListener(this)
         binding.leftArrowImageView.setOnClickListener(this)
@@ -107,10 +63,10 @@ class OrganizationInfoFragment<T>(organizationData: T?) : Fragment(),View.OnClic
                 val isShow = checkVisibilityOfProceed(connectionType)
                 if(isShow)
                 {
-                    addListnersToProceed()
+                    addListenersToProceed()
                 }
             } else {
-                removeListnersToProceed()
+                removeListenersToProceed()
             }
         }
 
@@ -120,9 +76,9 @@ class OrganizationInfoFragment<T>(organizationData: T?) : Fragment(),View.OnClic
                 val isShow = checkVisibilityOfProceed(connectionType)
                 if(isShow)
                 {
-                    addListnersToProceed()
+                    addListenersToProceed()
                 }else{
-                    removeListnersToProceed()
+                    removeListenersToProceed()
                 }
             }
             override fun afterTextChanged(s: Editable?) {}
@@ -134,9 +90,9 @@ class OrganizationInfoFragment<T>(organizationData: T?) : Fragment(),View.OnClic
                 val isShow = checkVisibilityOfProceed(connectionType)
                 if(isShow)
                 {
-                    addListnersToProceed()
+                    addListenersToProceed()
                 }else{
-                    removeListnersToProceed()
+                    removeListenersToProceed()
                 }
             }
             override fun afterTextChanged(s: Editable?) {}
@@ -160,26 +116,45 @@ class OrganizationInfoFragment<T>(organizationData: T?) : Fragment(),View.OnClic
         passwordEditText.setSelection(passwordEditText.text.length)
     }
 
-    private fun removeListnersToProceed() {
+    private fun removeListenersToProceed() {
         val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.rounded_rectangle_grey)
         binding.frameLayoutProceed.background = drawable
         binding.frameLayoutProceed.setOnClickListener(null)
     }
 
-    private fun addListnersToProceed() {
+    private fun addListenersToProceed() {
+        if(authType == null){
+            Log.d("addListenersToProceed", "prevented listener add - auth type is null.")
+            removeListenersToProceed()
+            return
+        }
+
+        if(!binding.checkbox.isChecked) {
+            Log.d("addListenersToProceed", "prevented listener add - attestation not checked")
+            removeListenersToProceed()
+            return
+        }
+
         val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.rounded_rectangle_green)
         binding.frameLayoutProceed.background = drawable
-        binding.frameLayoutProceed.setOnClickListener(this)
+
+        when(authType) {
+            ConnectionCategory.BASIC -> binding.frameLayoutProceed.setOnClickListener { onClickProceedBasic() }
+            ConnectionCategory.OAUTH -> binding.frameLayoutProceed.setOnClickListener { onClickProceedOAuth() }
+            null -> {
+                Log.w("addListenersToProceed", "tried to set listeners with null authType")
+                return
+            }
+        }
     }
 
     private fun checkVisibilityOfProceed(connectionType:String?): Boolean {
-        if(connectionType.equals(resources.getString(R.string.hapi)))
-        {
-            return !(binding.editTextUsername.text.toString() == "" || binding.editTextPassword.text.toString().equals("") || !binding.checkbox.isChecked)
+        // TODO:Question - We should probably always be able to proceed? Either with the BASIC auth or to the OAuth URL, right? Or is this connectionType not the same as AuthType?
+        return if(connectionType.equals(resources.getString(R.string.hapi))) {
+            !(binding.editTextUsername.text.toString() == "" || binding.editTextPassword.text.toString() == "" || !binding.checkbox.isChecked)
         }else{
-            return true
+            true
         }
-
     }
 
     override fun onDestroyView() {
@@ -201,87 +176,142 @@ class OrganizationInfoFragment<T>(organizationData: T?) : Fragment(),View.OnClic
                 parentFragmentManager.popBackStack()
             }
             R.id.frameLayoutProceed -> {
-                val parentFrag: DataConnectionsFragment = this@OrganizationInfoFragment.parentFragment as DataConnectionsFragment
-                Log.d("Vict organization", "$organization")
+                // should never get here anymore!!
+            }
+        }
+    }
 
-                val orgId = getId(organization)
-                    ?: throw Exception("OrgId was null. Bad things are happening here.")
+    private fun onClickProceedOAuth(){
+        // do the OAuth things here
+        Log.d("onClickProceedOAuth", "Hey there, would you like some OAuth today?")
 
-//                Log.d("Organization.Id:", orgId)
-//                val connectionData = parentFrag.getDataSource(orgId)//"55a83bdc8d1eb1420aa1a71b")
-//                GlobalScope.launch (Dispatchers.Main) {
-//                    dataSource = getDataSource(connectionData)
-//
-//                    Log.d("dataSource.type", dataSource?.type.toString())
-//                    Log.d("dataSource.name", dataSource!!.name)
-//                    Log.d("dataSource.id", dataSource!!.id)
-//                    Log.d("dataSource.category", dataSource!!.category.toString())
-//                }
-                // category Basic or OAuth
+        openOAuthView()
+    }
 
-                if (dataSource!!.category.toString().equals("OPEN", ignoreCase = true)) {
-                    val connectionCreateRequest: ConnectionCreateRequest = ConnectionCreateRequest.Builder()
-                        //.connectionId(connectionId)
-                        .connectionId("55a83bdc8d1eb1420aa1a71b")
-                        .username(binding.editTextUsername.text.toString())
-                        .password(binding.editTextPassword.text.toString())
-                        .build()
+    private fun onClickProceedBasic(){
+        // do the Basic things here
+        Log.d("onClickProceedBasic", "Basically, lets proceed")
+    }
 
-                    parentFrag.createConnection(connectionCreateRequest)
+    private fun showLogin(){
+        // show different login components based on dataSource Connection type
+        Log.d("showLogin", "Begin")
+        viewModel.getDataSource(entityId)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.dataSourceData.collect { result ->
+                if(result != null){
+                    Log.d("showLogin", "collected dataSource")
+                    // based on data source connection, either show basic or oauth login
+                    val dataSource = (result as? BWellResult.SingleResource<DataSource>)?.data ?: throw Exception("Collected null dataSource")
 
-                } else if (connectionType.equals("OAUTH", ignoreCase = true)) {
-                    // Handle the case when connectionType is "oAuth"
-                    parentFrag.getOAuthUrl("epic_sandbox_r4c")
-                    //openUrl()
+                    Log.d("showLogin", "dataSource.category: ${dataSource.category}")
+                    authType = dataSource.category
+                    dataSourceId = dataSource.id
+                    // add Listeners to Proceed now that we know our auth type
+                    addListenersToProceed()
+                    if( dataSource.category == ConnectionCategory.BASIC ) {
+                        showBasicLogin()
+                    } else if( dataSource.category == ConnectionCategory.OAUTH ){
+                        showOAuthLogin()
+                    }
                 }
             }
         }
     }
 
-    private suspend fun getDataSource(orgId: String): DataSource {
-        viewModel.getDataSource(orgId);
-
-        return (viewModel.dataSourceData.firstOrNull() as? BWellResult.SingleResource<DataSource>)?.data
-            ?: throw Exception("Could not get dataSource. Sadness...")
+    private fun showBasicLogin(){
+        Log.d("showBasicLogin", "Begin")
+        // set text
+        binding.clinicNametxt.text = resources.getString(R.string.connect_to_entity, entityName)
+        binding.clinicDescriptionTxt.text = resources.getString(R.string.clinic_info_hapi, entityName)
+        binding.editTextUsername.visibility = View.VISIBLE
+        binding.passwordLayout.visibility = View.VISIBLE
+        Log.d("showBasicLogin", "End")
     }
 
-    private suspend fun getOAuthUrl(dataSourceId: String): String {
-        viewModel.getOAuthUrl(dataSourceId);
+    private fun showOAuthLogin(){
+        Log.d("showOAuthLogin", "Begin")
 
-        return (viewModel.oauthUrlData.firstOrNull() as? BWellResult.SingleResource<String>)?.data
-            ?: throw Exception("Could not get oauthUrl. Sadness...")
+        binding.clinicNametxt.text = resources.getString(R.string.connect_to_entity, entityName)
+        binding.clinicDescriptionTxt.text = resources.getString(R.string.clinic_description, entityName) // TODO this description references Lee Health TOS
+        binding.editTextUsername.visibility = View.GONE
+        binding.passwordLayout.visibility = View.GONE
+        // TODO:Question - what about these?
+        // binding.checkbox.visibility = View.GONE
+        // binding.checkboxTxt.visibility = View.GONE
+
+        Log.d("showOAuthLogin", "End")
     }
 
+    private fun openOAuthView(){
+        Log.d("openOAuthView", "Begin")
+        viewModel.getOAuthUrl(dataSourceId ?: throw Exception("Tried to open an oauthUrl before dataSource found"))
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.oauthUrlData.collect { result ->
+                if(result != null){
+                    Log.d("openOAuthView", "collected oauthData")
+                    // openUrl
+                    val oauthData = (result as? BWellResult.SingleResource<String>)?.data ?: throw Exception("No OAuth Url available for OAuth data connection entity")
 
-    fun getId(obj:T?): String? {
-        if(obj is Organization?){
-            val org = obj as Organization?
-
-            if(org != null){
-                // TODO: Use the right filter here
-                val endpoint = org.endpoint?.filter {
-                        ep -> ep?.identifier?.any {
-                        id -> id?.system ==  "https://integrationhub-web.prod.bwell.zone/connectionhub/clientconnections/oid" }
-                    ?: return "" }
-                    ?.first()
-
-                return endpoint?.name
+                    // do something here:
+                    Log.d("OAuth Url", oauthData)
+                }
             }
-        } else if(obj is Provider?){
-            val prov = obj as Provider?
+        }
+    }
 
-            if(prov != null){
-                // TODO: Use the right filter here
-                val endpoint = prov.endpoint?.filter {
-                        ep -> ep?.identifier?.any {
-                        id -> id?.system ==  "https://integrationhub-web.prod.bwell.zone/connectionhub/clientconnections/oid" }
-                    ?: return "" }
-                    ?.first()
-
-                return endpoint?.name
+    private fun getConnectionType(entity: T?): String {
+        when (val nonNullEntity = requireNotNull(entity) { "Entity cannot be null in OrganizationInfoFragment" }) {
+            is Organization -> {
+                return nonNullEntity.endpoint?.get(0)?.connectionType?.code.toString()
+            }
+            is Provider -> {
+                return nonNullEntity.endpoint?.get(0)?.connectionType?.code.toString()
             }
         }
 
-        return null
+        throw IllegalStateException("Could not get connection type. Must be either Provider or Organization.")
+    }
+
+    private fun getName(entity: T?): String {
+        when (val nonNullEntity = requireNotNull(entity) { "Entity cannot be null in OrganizationInfoFragment" }) {
+            is Organization -> {
+                return nonNullEntity.name.toString()
+            }
+            is Provider -> {
+                return nonNullEntity.content.toString()
+            }
+        }
+
+        throw IllegalStateException("Could not get entity name. Must be either Provider or Organization.")
+    }
+
+    private fun getId(entity:T?): String {
+
+        when (val nonNullEntity = requireNotNull(entity) { "Entity cannot be null in OrganizationInfoFragment" }) {
+            is Organization -> {
+                // TODO: Use the right filter here
+                val endpoint = nonNullEntity.endpoint?.filter {
+                        ep -> ep?.identifier?.any {
+                        id -> id?.system ==  "https://integrationhub-web.prod.bwell.zone/connectionhub/clientconnections/oid" }
+                    ?: return "" }
+                    ?.first() ?: throw Exception("No clientConnections endpoint present on Organization entity.")
+
+                return endpoint.name ?: throw Exception("No name present on Organization endpoint")
+            }
+            is Provider -> {
+                // TODO: Use the right filter here
+                val endpoint = nonNullEntity.endpoint?.filter {
+                        ep -> ep?.identifier?.any {
+                        id -> id?.system ==  "https://integrationhub-web.prod.bwell.zone/connectionhub/clientconnections/oid" }
+                    ?: return "" }
+                    ?.first() ?: throw Exception("No clientConnections endpoint present on Provider entity.")
+
+                return endpoint.name ?: throw Exception("No name present on Provider endpoint")
+            }
+        }
+
+        // Should not get here
+        throw IllegalStateException("Could not get entity id. Must be either Provider or Organization.")
     }
 }
