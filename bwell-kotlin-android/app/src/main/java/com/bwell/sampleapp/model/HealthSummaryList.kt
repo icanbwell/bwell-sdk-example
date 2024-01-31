35ebcb7ff0f2a6c0c8258b9bc246433d4b016f88
Jerry Westberg <jerry.westberg@icanbwell.com>
2024-01-31 16:06:30 -0700

    Call SDK for health summary. Fixed double click bug on health summary.

--- bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/model/HealthSummaryList.kt
+++ bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/model/HealthSummaryList.kt
@@ -1,10 +1,14 @@
 package com.bwell.sampleapp.model
 
-import com.bwell.common.models.domain.common.Period
+import com.bwell.common.models.domain.healthdata.healthsummary.healthsummary.enums.HealthSummaryCategory
 
 data class HealthSummaryList(
     val healthSummaryList :List<HealthSummaryListItems>
 )
 
-data class HealthSummaryListItems(val healthSummaryTypeLogo: Int, val healthSummaryDetailsLogo: Int, val category: String?,
-                                  val date: Period)
+data class HealthSummaryListItems(
+    val healthSummaryTypeLogo: Int,
+    val healthSummaryDetailsLogo: Int,
+    val category: HealthSummaryCategory,
+    val categoryFriendlyName: String?,
+    val count: Int?)
