3cf902e1bb08b27db8fbc5d472ee95b26f38431e
katekolchier <77307368+katekolchier@users.noreply.github.com>
2024-04-02 10:51:33 -0500

    EA-1152 Updates for 1.0.0 (#51)

--- bwell-kotlin-android/app/build.gradle.kts
+++ bwell-kotlin-android/app/build.gradle.kts
@@ -67,7 +67,7 @@ android {
 dependencies {
 
     // BWell SDK Usage
-    implementation("com.bwell:bwell-sdk-kotlin:0.20.0")
+    implementation("com.bwell:bwell-sdk-kotlin:1.0.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
