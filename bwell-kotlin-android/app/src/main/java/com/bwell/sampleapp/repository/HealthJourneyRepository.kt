cb35fb8931e997326648b25b7ad26b8b5b3d8b5a
grant <gespanet97@gmail.com>
2024-04-02 11:43:57 -0400

    EA-1055 Update TaskStatus (#49)
    * implement update task status
    
    * refresh UI upon task status update

--- bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/repository/HealthJourneyRepository.kt
+++ bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/repository/HealthJourneyRepository.kt
@@ -2,6 +2,7 @@ package com.bwell.sampleapp.repository
 
 import com.bwell.sampleapp.singletons.BWellSdk
 import com.bwell.activity.requests.TasksRequest
+import com.bwell.activity.requests.UpdateTaskRequest
 import com.bwell.common.models.domain.task.Task
 import com.bwell.common.models.responses.BWellResult
 import kotlinx.coroutines.flow.Flow
@@ -15,6 +16,14 @@ class HealthJourneyRepository {
             emit(taskResult)
         } catch (e: Exception) {
             emit(null)
+        }
+    }
+
+    suspend fun updateTask(updateTaskRequest: UpdateTaskRequest): BWellResult<Task>? {
+        return try {
+            BWellSdk.activity.updateTaskStatus(updateTaskRequest)
+        } catch (e: Exception) {
+            null
         }
     }
 }
