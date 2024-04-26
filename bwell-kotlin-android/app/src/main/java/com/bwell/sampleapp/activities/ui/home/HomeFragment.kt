9e6cf8544d80b188d8302cae283090bb8464a9be
Carter Wooten <carter.wooten@icanbwell.com>
2024-04-26 16:50:51 -0400

    EA-1171 Updates for handleNotification

--- bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/activities/ui/home/HomeFragment.kt
+++ bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/activities/ui/home/HomeFragment.kt
@@ -2,7 +2,6 @@ package com.bwell.sampleapp.activities.ui.home
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,27 +10,17 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.bwell.device.requests.deviceToken.DevicePlatform
-import com.bwell.device.requests.deviceToken.RegisterDeviceTokenRequest
 import com.bwell.sampleapp.BWellSampleApplication
 import com.bwell.sampleapp.R
 import com.bwell.sampleapp.databinding.FragmentHomeBinding
 import com.bwell.sampleapp.model.ActivityListItems
 import com.bwell.sampleapp.repository.Repository
-import com.bwell.sampleapp.utils.getEncryptedSharedPreferences
 import com.bwell.sampleapp.viewmodel.SharedViewModel
 import com.bwell.sampleapp.viewmodel.SharedViewModelFactory
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
-import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import org.json.JSONObject
 
 
 class HomeFragment : Fragment(), View.OnClickListener {
-    private val TAG = "HomeFragment"
     private var _binding: FragmentHomeBinding? = null
 
     private val binding get() = _binding!!
@@ -62,39 +51,7 @@ class HomeFragment : Fragment(), View.OnClickListener {
                 binding.userName.text = resources.getString(R.string.welcome_bwell)+" "+it?.firstName+"!"
             }
         }
-
-        registerDeviceToken()
-
         return root
-    }
-
-    private fun registerDeviceToken() {
-        CoroutineScope(Dispatchers.IO).launch {
-            val deferred = CompletableDeferred<String>()
-            FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-                if (!task.isSuccessful) {
-                    Log.w(TAG, "Fetching FCM registration token failed", task.exception)
-                    return@OnCompleteListener
-                }
-                deferred.complete(task.result)
-            })
-            val deviceToken = deferred.await()
-            val registerDeviceTokenRequest: RegisterDeviceTokenRequest = RegisterDeviceTokenRequest.Builder()
-                .deviceToken(deviceToken)
-                .applicationName("com.bwell.sampleapp")
-                .platform(DevicePlatform.ANDROID)
-                .build()
-            val registerOutcome = repository.registerDeviceToken(registerDeviceTokenRequest)
-            registerOutcome.collect { outcome ->
-                outcome?.let {
-                    if (outcome.success()) {
-                        println("FCM_TOKEN Registered Successfully")
-                    } else {
-                        println("FCM_TOKEN Failed to register")
-                    }
-                }
-            }
-        }
     }
 
     private fun setAdapter(suggestedActivitiesLIst: List<ActivityListItems>) {
--- bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/utils/BWellFirebaseMessagingService.kt
+++ bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/utils/BWellFirebaseMessagingService.kt
@@ -19,63 +19,23 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.json.JSONObject
+import java.io.Serializable
 
 
 private const val CHANNEL_ID = "1"
 
 
 class BWellFirebaseMessagingService : FirebaseMessagingService() {
-    private val TAG = "BWellFirebaseMessagingService"
-
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        if (remoteMessage.data?.isNotEmpty() == true) {
-            // Use a coroutine to fire and forget `handleNotification`
-            CoroutineScope(Dispatchers.IO).launch {
-                try {
-                    Log.i(TAG, "Capturing notification and passing it to SDK for processing.")
-                    val response = BWellSdk.event.handleNotification(JSONObject(remoteMessage.data as Map<String, String>).toString())
-                    if (response.success()) {
-                        Log.i(TAG, "BwellSdk.events.handleNotification successful.")
-                    } else {
-                        throw Exception(response.message())
-                    }
-                } catch(error: Exception) {
-                    // Swallow the error but log it out so it's captured somewhere
-                    Log.e(TAG, "There was an error when attempting to handle incoming notification.", error)
-                }
-            }
+        if (remoteMessage.data.isNotEmpty()) {
+            val title = remoteMessage.data["title"] ?: ""
+            val body = remoteMessage.data["body"] ?: ""
 
-            val data: Map<String, String> = remoteMessage.data
-            val notificationId = data["notification_id"]
-            val title = data["title"] ?: ""
-            val body = data["body"] ?: ""
-            val actionType = data["action_type"] ?: ""
-            val action = data["action"] ?: ""
-
-            createNotification(title, body, action, actionType)
+            createNotification(title, body, remoteMessage.data)
         }
     }
 
-    override fun onNewToken(token: String) {
-        super.onNewToken(token)
-        storeDeviceToken(token)
-    }
-
-    private fun storeDeviceToken(token: String) {
-        // save the fcm token in encrypted shared preferences
-        val encryptedPreferences = getEncryptedSharedPreferences(applicationContext)
-        val encryptedEditor = encryptedPreferences.edit()
-        encryptedEditor.putString(R.string.fcm_device_token.toString(), token)
-        encryptedEditor.apply()
-
-        // save a flag indicating that the new fcm token has not been registered
-        val sharedPreferences = getSharedPreferences(applicationContext)
-        val editor = sharedPreferences.edit()
-        editor.putBoolean(R.string.fcm_device_token_registered.toString(), false)
-        editor.apply()
-    }
-
-    private fun createNotification(title: String, body: String, action: String, actionType: String) {
+    private fun createNotification(title: String, body: String, remoteMessageData: Map<String, String>) {
         val notificationBuilder: NotificationCompat.Builder = NotificationCompat.Builder(this, Notification(
         ))
             .setContentTitle(title)
@@ -85,14 +45,12 @@ class BWellFirebaseMessagingService : FirebaseMessagingService() {
             .setSilent(true)
             .setSmallIcon(coil.base.R.drawable.notification_icon_background)
 
-        if (actionType == "deep_link") {
+        if (remoteMessageData["action_type"] == "deep_link") {
             var resultIntent: Intent? = null
-            if (action.startsWith("ActivityDefinition/")) {
-                resultIntent = Intent(this, NavigationActivity::class.java)
-                val bundle = Bundle().apply {
-                    putString("action", action)
+            if (remoteMessageData["action"]?.startsWith("ActivityDefinition/") == true) {
+                resultIntent = Intent(this, NavigationActivity::class.java).apply {
+                    putExtra("remoteMessageDataStringified", JSONObject(remoteMessageData).toString())
                 }
-                resultIntent.putExtras(bundle)
             }
 
             if (resultIntent != null) {
