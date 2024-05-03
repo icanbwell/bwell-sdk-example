90b4cc6a06a58d3d904ec3f95fa57ced47f4e63e
Kate Kolchier <kate.kolchier@icanbwell.com>
2024-05-03 05:56:37 -0500

    EA-1235 Updated SDK version to 1.1.1

--- bwell-kotlin-android/app/build.gradle.kts
+++ bwell-kotlin-android/app/build.gradle.kts
@@ -67,7 +67,7 @@ android {
 dependencies {
 
     // BWell SDK Usage
-    implementation("com.bwell:bwell-sdk-kotlin:1.1.0")
+    implementation("com.bwell:bwell-sdk-kotlin:1.1.1")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
