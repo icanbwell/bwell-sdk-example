0f5e7454435194a7097c7896dbaa0bad3de5f501
Kyle Wade <kyle.wade@icanbwell.com>
2024-05-23 16:37:19 +0000

    Remove logging

--- bwell-typescript-react/src/sdk/bWellSdk.ts
+++ bwell-typescript-react/src/sdk/bWellSdk.ts
@@ -1,7 +1,5 @@
 import { BWellSDK } from '@icanbwell/bwell-sdk-ts';
 
-console.info('Instantiating SDK...');
-
 export let bWellSdk: BWellSDK;
 
 export const initializeSdk = async (clientKey: string) => {
