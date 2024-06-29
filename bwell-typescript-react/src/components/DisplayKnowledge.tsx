67663f70d8999a5c86285ab64f04ea80adc0917c
Kyle Wade <kyle.wade@icanbwell.com>
2024-06-29 14:58:27 -0400

    Add HTML-JSON toggle to DisplayKnowledge

--- bwell-typescript-react/src/components/DisplayKnowledge.tsx
+++ bwell-typescript-react/src/components/DisplayKnowledge.tsx
@@ -1,4 +1,7 @@
-import { Box, Container } from "@mui/material"
+import { RootState } from "@/store/store";
+import { selectToggle, toggleValue } from "@/store/toggleSlice";
+import { Box, Container, ToggleButton, ToggleButtonGroup } from "@mui/material"
+import { useDispatch, useSelector } from "react-redux";
 
 interface Resource {
     content: string;
@@ -16,16 +19,29 @@ interface DisplayKnowledgeProps {
 }
 
 export const DisplayKnowledge: React.FC<DisplayKnowledgeProps> = ({ name, healthData }) => {
+    const TOGGLE_LOCATOR = `${name}-knowledge`;
+
+    const showHtml = useSelector((state: RootState) => selectToggle(state, TOGGLE_LOCATOR)) && healthData?.data?.resources?.length;
+
+    const dispatch = useDispatch();
+
+    const handleToggleView = () => {
+        dispatch(toggleValue(TOGGLE_LOCATOR));
+    };
+
     return <Container>
-        {healthData?.data?.resources?.length &&
-            <>
-                <h2>{name} Knowledge: HTML</h2>
-                <div dangerouslySetInnerHTML={{ __html: healthData.data.resources?.map(r => r.content).join('') }} />
-            </>
-        }
-        <h2>{name} Knowledge: JSON</h2>
-        <Box>
-            <pre style={{ textWrap: 'wrap' }}>{JSON.stringify(healthData, null, 2)}</pre>
+        <h2>{name} Knowledge</h2>
+        <Box sx={{ padding: '5px' }}>
+            <ToggleButtonGroup
+                value={showHtml ? 'html' : 'json'}
+                exclusive
+                onChange={handleToggleView}
+            >
+                <ToggleButton value="html">HTML</ToggleButton>
+                <ToggleButton value="json">JSON</ToggleButton>
+            </ToggleButtonGroup>
         </Box>
+        {showHtml && <div dangerouslySetInnerHTML={{ __html: healthData.data.resources?.map(r => r.content).join('') }} />}
+        {!showHtml && <pre style={{ textWrap: 'wrap' }}>{JSON.stringify(healthData, null, 2)}</pre>}
     </Container>
 }
