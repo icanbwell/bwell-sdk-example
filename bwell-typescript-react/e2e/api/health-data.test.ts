bf6240156ec07270d7c60720921b348eb8e553f7
Kyle Wade <kyle.wade@icanbwell.com>
2024-08-01 13:32:27 +0000

    Refactor SDK to beforeAll, add a test for a successful request

--- bwell-typescript-react/e2e/api/health-data.test.ts
+++ bwell-typescript-react/e2e/api/health-data.test.ts
@@ -1,12 +1,14 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeAll } from 'vitest';
 import { AllergyIntolerancesRequest, BWellSDK } from '@icanbwell/bwell-sdk-ts';
 
 const DEFAULT_KEY = process.env.VITE_DEFAULT_KEY ?? "";
 const DEFAULT_OAUTH_CREDS = process.env.VITE_DEFAULT_OAUTH_CREDS ?? "";
 
+let sdk: BWellSDK;
+
 describe("SDK Health Data", () => {
-    it("should fail validation if given an empty code", async () => {
-        const sdk = new BWellSDK({
+    beforeAll(async () => {
+        sdk = new BWellSDK({
             clientKey: DEFAULT_KEY,
         });
 
@@ -15,7 +17,9 @@ describe("SDK Health Data", () => {
         await sdk.authenticate({
             token: DEFAULT_OAUTH_CREDS,
         });
+    });
 
+    it("should fail validation if given an empty code", async () => {
         const request = new AllergyIntolerancesRequest({
             page: 0,
             pageSize: 10,
@@ -33,5 +37,18 @@ describe("SDK Health Data", () => {
         expect(result).toBeDefined();
         expect(result.hasError()).toBe(true);
         expect(result.error?.message).toBe('Each Coding in the list must have at least one non-null and non-empty value for system or code');
+    });
+
+    it("should pass validation when given minimal request", async () => {
+        const request = new AllergyIntolerancesRequest({
+            page: 0,
+            pageSize: 10,
+        });
+
+        const result = await sdk.health.getAllergyIntolerances(request);
+
+        expect(result).toBeDefined();
+        expect(result.hasError()).toBe(false);
+        expect(result.data).toBeDefined();
     });
 });
