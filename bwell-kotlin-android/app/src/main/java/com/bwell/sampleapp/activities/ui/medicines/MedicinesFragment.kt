a853cbe9026ba4671f70715c03b59325c111ac2b
Aushwin Biju <aushwin.biju@icanbwell.com>
2024-11-21 15:26:58 +0530

    ATHD-8302 feat: Integrate medication pricing and create pricing view

--- bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/activities/ui/medicines/MedicinesFragment.kt
+++ bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/activities/ui/medicines/MedicinesFragment.kt
@@ -3,6 +3,7 @@ package com.bwell.sampleapp.activities.ui.medicines
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
