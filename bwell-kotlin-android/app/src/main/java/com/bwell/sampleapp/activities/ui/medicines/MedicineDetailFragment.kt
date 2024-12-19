2f97c9c13d5dc78eb15173d8fea9564ae443dca4
Aushwin <aushwin.biju@icanbwell.com>
2024-12-19 10:48:24 +0530

    ATHD-8302 feat: Integrate medication pricing and create pricing view (#66)
    * ATHD-8302 feat: Integrate medication pricing and create pricing view
    
    * ATHD-8302 chore: Remove debug logs
    
    * ATHD-8302 bump: version bump to latest pre-release and commenting mavenLocal
    
    ---------
    
    Co-authored-by: kylewadebwell <167116073+kylewadebwell@users.noreply.github.com>

--- bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/activities/ui/medicines/MedicineDetailFragment.kt
+++ bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/activities/ui/medicines/MedicineDetailFragment.kt
@@ -5,16 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bwell.common.models.domain.common.Coding
+import com.bwell.common.models.domain.healthdata.medication.MedicationGroup
+import com.bwell.common.models.domain.healthdata.medication.MedicationPricing
 import com.bwell.common.models.domain.healthdata.medication.MedicationStatement
 import com.bwell.common.models.requests.searchtoken.SearchDate
 import com.bwell.common.models.responses.BWellResult
 import com.bwell.healthdata.medication.requests.MedicationKnowledgeRequest
+import com.bwell.healthdata.medication.requests.MedicationPricingRequest
 import com.bwell.healthdata.medication.requests.MedicationStatementsRequest
 import com.bwell.sampleapp.BWellSampleApplication
 import com.bwell.sampleapp.R
@@ -22,7 +25,6 @@ import com.bwell.sampleapp.databinding.MedicineDetailViewBinding
 import com.bwell.sampleapp.utils.formatDate
 import com.bwell.sampleapp.viewmodel.MedicineViewModelFactory
 import com.bwell.sampleapp.viewmodel.MedicinesViewModel
-import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -36,6 +38,11 @@ class MedicineDetailFragment : Fragment(),View.OnClickListener {
     private lateinit var groupSystem: String
     private lateinit var name: String
     private var from: String? = null
+
+    private var medicationStatementId: String? = null
+
+    private lateinit var medicationPricingListAdapter: MedicationPricingListAdapter
+
 
     private val binding get() = _binding!!
 
@@ -55,6 +62,8 @@ class MedicineDetailFragment : Fragment(),View.OnClickListener {
         groupSystem = arguments?.getString("groupSystem").toString()
         name = arguments?.getString("name").toString()
         from = arguments?.getString("from")
+
+
 
         showOverView()
 
@@ -79,19 +88,29 @@ class MedicineDetailFragment : Fragment(),View.OnClickListener {
             R.id.whatIsItTextView -> {
                showKnowledgeView()
             }
+            R.id.pricingTextView -> {
+                showPricingView()
+            }
         }
     }
 
     private fun showOverView() {
         binding.medicineOverviewView.medicineOverviewView.visibility = View.VISIBLE
         binding.medicineKnowledgeView.medicineKnowledgeView.visibility = View.GONE
+        binding.medicinePricingView.rvPricingView.visibility = View.GONE
+
         binding.whatIsItTextView.setTextColor(resources.getColor(R.color.black))
         binding.whatIsItunderline.setBackgroundColor(resources.getColor(R.color.medicine_tabs_non_selected_color))
         binding.overviewTextView.setTextColor(resources.getColor(R.color.medicine_tabs_selected_color))
         binding.overviewunderline.setBackgroundColor(resources.getColor(R.color.medicine_tabs_selected_color))
+        binding.pricingTextView.setTextColor(resources.getColor(R.color.black))
+        binding.pricingUnderline.setBackgroundColor(resources.getColor(R.color.medicine_tabs_non_selected_color))
+
         binding.overviewTextView.setOnClickListener(null)
         binding.whatIsItTextView.setOnClickListener(this)
+        binding.pricingTextView.setOnClickListener(this)
         binding.medicineKnowledgeView.containerLayout.removeAllViews()
+        binding.medicinePricingView.rvPricingView.removeAllViews()
 
         val lastUpdatedSearchDate = SearchDate.Builder()
             .greaterThan(SimpleDateFormat("yyyy-MM-dd").parse("2020-02-12"))
@@ -107,6 +126,7 @@ class MedicineDetailFragment : Fragment(),View.OnClickListener {
             medicinesViewModel.medicationStatementsResults.collect { result ->
                 if (result != null && result is BWellResult.ResourceCollection<MedicationStatement>) {
                     val medicationStatement = result.data?.firstOrNull()
+                    medicationStatementId = medicationStatement?.id
                     binding.medicineOverviewView.medicineTitleTextView.text = name
                     binding.medicineOverviewView.medicationValueTextView.text = medicationStatement?.medication?.coding?.firstOrNull()?.display ?: medicationStatement?.medication?.coding?.firstOrNull()?.code?.capitalize(
                         Locale.ROOT)
@@ -127,15 +147,25 @@ class MedicineDetailFragment : Fragment(),View.OnClickListener {
     private fun showKnowledgeView() {
         binding.medicineOverviewView.medicineOverviewView.visibility = View.GONE
         binding.medicineKnowledgeView.medicineKnowledgeView.visibility = View.VISIBLE
+        binding.medicinePricingView.rvPricingView.visibility = View.GONE
+
         binding.whatIsItTextView.setTextColor(resources.getColor(R.color.medicine_tabs_selected_color))
         binding.whatIsItunderline.setBackgroundColor(resources.getColor(R.color.medicine_tabs_selected_color))
         binding.overviewTextView.setTextColor(resources.getColor(R.color.black))
         binding.overviewunderline.setBackgroundColor(resources.getColor(R.color.medicine_tabs_non_selected_color))
+        binding.pricingTextView.setTextColor(resources.getColor(R.color.black))
+        binding.pricingUnderline.setBackgroundColor(resources.getColor(R.color.medicine_tabs_non_selected_color))
+
+
         binding.overviewTextView.setOnClickListener(this)
         binding.whatIsItTextView.setOnClickListener(null)
+        binding.pricingTextView.setOnClickListener(this)
+
         binding.medicineKnowledgeView.containerLayout.removeAllViews()
+        binding.medicinePricingView.rvPricingView.removeAllViews()
+
         val request = MedicationKnowledgeRequest.Builder()
-            .medicationStatementId(medicationId)
+            .medicationStatementId(medicationStatementId?:medicationId)
             .build()
         medicinesViewModel.getMedicationKnowledge(request)
         viewLifecycleOwner.lifecycleScope.launch {
@@ -157,4 +187,58 @@ class MedicineDetailFragment : Fragment(),View.OnClickListener {
             }
         }
     }
-}
+
+    private fun setMedicinesGroupsAdapter(result:BWellResult<MedicationPricing>) {
+        when (result) {
+            is BWellResult.ResourceCollection -> {
+                val dataList = result.data
+                medicationPricingListAdapter = MedicationPricingListAdapter(dataList)
+                binding.medicinePricingView.rvPricingView.layoutManager =
+                    LinearLayoutManager(requireContext())
+                binding.medicinePricingView.rvPricingView.adapter = medicationPricingListAdapter
+            }
+
+            else -> {}
+        }
+    }
+
+
+
+        private fun showPricingView() {
+        binding.medicineOverviewView.medicineOverviewView.visibility = View.GONE
+        binding.medicineKnowledgeView.medicineKnowledgeView.visibility = View.GONE
+        binding.medicinePricingView.rvPricingView.visibility = View.VISIBLE
+
+        binding.pricingTextView.setTextColor(resources.getColor(R.color.medicine_tabs_selected_color))
+        binding.pricingUnderline.setBackgroundColor(resources.getColor(R.color.medicine_tabs_selected_color))
+
+        binding.overviewTextView.setTextColor(resources.getColor(R.color.black))
+        binding.overviewunderline.setBackgroundColor(resources.getColor(R.color.medicine_tabs_non_selected_color))
+        binding.whatIsItTextView.setTextColor(resources.getColor(R.color.black))
+        binding.whatIsItunderline.setBackgroundColor(resources.getColor(R.color.medicine_tabs_non_selected_color))
+
+        binding.overviewTextView.setOnClickListener(this)
+        binding.whatIsItTextView.setOnClickListener(this)
+        binding.pricingTextView.setOnClickListener(null)
+
+        binding.medicinePricingView.rvPricingView.removeAllViews()
+        val request = MedicationPricingRequest.Builder()
+            .medicationStatementId(medicationStatementId?:medicationId)
+            .build()
+        medicinesViewModel.getMedicationPricing(request)
+        viewLifecycleOwner.lifecycleScope.launch {
+            medicinesViewModel.medicationPricingResults.collect { result ->
+                if (result != null) {
+                    when (result) {
+                        is BWellResult.ResourceCollection -> {
+                            setMedicinesGroupsAdapter(result)
+                        }
+
+                        else -> {}
+                    }
+                }
+            }
+        }
+    }
+
+    }
