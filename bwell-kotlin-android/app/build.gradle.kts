ad70d1bc174a8ca12a07793070f02eb497d61d97
Nirbhay Kumar Nachiketa <nirbhay.nachiketa@icanbwell.com>
2025-08-05 17:59:17 +0530

    PHR-1372 Update the kotlin sdk version to 1.7.0

--- bwell-kotlin-android/app/build.gradle.kts
+++ bwell-kotlin-android/app/build.gradle.kts
@@ -67,7 +67,7 @@ android {
 dependencies {
 
     // BWell SDK Usage
-    implementation("com.bwell:bwell-sdk-kotlin:1.6.0")
+    implementation("com.bwell:bwell-sdk-kotlin:1.7.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
