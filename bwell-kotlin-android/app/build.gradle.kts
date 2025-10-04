c652a44afbe9b5cc3ac1db88cb94eb5db65ccd9a
Kate Kolchier <kate.kolchier@icanbwell.com>
2025-10-03 20:12:59 -0500

    Bump SDK version to 1.9.0

--- bwell-kotlin-android/app/build.gradle.kts
+++ bwell-kotlin-android/app/build.gradle.kts
@@ -69,7 +69,7 @@ android {
 dependencies {
 
     // BWell SDK Usage
-    implementation("com.bwell:bwell-sdk-kotlin:1.8.0")
+    implementation("com.bwell:bwell-sdk-kotlin:1.9.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
