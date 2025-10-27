1d6f8b334731b0f593621612bf2926ab8c5b0e6b
Ivan Villanueva <ivan.villanueva@dxc.com>
2025-10-27 09:45:06 -0600

    Created project for sample app

---
+++ bwell-swift-ios/bwell-swift-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bwell-swift-ios
+//
+//  Created by Ivan Villanueva on 27/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bwell-swift-ios/bwell-swift-iosTests/bwell_swift_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  bwell_swift_iosTests.swift
+//  bwell-swift-iosTests
+//
+//  Created by Ivan Villanueva on 27/10/25.
+//
+
+import Testing
+@testable import bwell_swift_ios
+
+struct bwell_swift_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
