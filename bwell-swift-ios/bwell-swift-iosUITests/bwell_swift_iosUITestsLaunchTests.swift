65bbaafbd86e513f6c9fa25cc3babde6e750ae3a
Ivan Villanueva <ivan.villanueva@dxc.com>
2025-10-27 09:45:06 -0600

    Created project for sample app

---
+++ bwell-swift-ios/bwell-swift-iosUITests/bwell_swift_iosUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  bwell_swift_iosUITestsLaunchTests.swift
+//  bwell-swift-iosUITests
+//
+//  Created by Ivan Villanueva on 27/10/25.
+//
+
+import XCTest
+
+final class bwell_swift_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
