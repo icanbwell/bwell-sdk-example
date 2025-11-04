595fe04c9d46c6089f798b809dec40b3d31e07a0
Nirbhay Kumar Nachiketa <nirbhay.nachiketa@icanbwell.com>
2025-11-04 16:02:58 +0530

    PHR-1810 Some fixes

--- bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/viewmodel/QuestionnaireViewModel.kt
+++ bwell-kotlin-android/app/src/main/java/com/bwell/sampleapp/viewmodel/QuestionnaireViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bwell.sampleapp.activities.ui.questionnaire.QuestionnaireItem
 import com.bwell.sampleapp.repository.QuestionnaireRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 /**
@@ -12,16 +13,13 @@ import kotlinx.coroutines.launch
  */
 class QuestionnaireViewModel(private val repository: QuestionnaireRepository?) : ViewModel() {
 
-    val questionnaireData: LiveData<List<QuestionnaireItem>>
-        get() = repository?.questionnaireData ?: throw IllegalStateException("Repository is null")
-
-    init {
-        loadQuestionnaires()
-    }
-
-    private fun loadQuestionnaires() {
-        viewModelScope.launch {
+    // Explicit call from Fragment (mirrors other explicit getX() usages elsewhere)
+    fun getQuestionnaireResponses() {
+        viewModelScope.launch(Dispatchers.IO) {
             repository?.getQuestionnaireList()
         }
     }
+
+    val questionnaireData: LiveData<List<QuestionnaireItem>>
+        get() = repository?.questionnaireData ?: throw IllegalStateException("Repository is null")
 }
